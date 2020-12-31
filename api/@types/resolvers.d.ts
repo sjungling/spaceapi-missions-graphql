465bc035d7a6341ad90cd1d5b34072a52c6d8663
Scott Jungling <scott.jungling@gmail.com>
2020-12-30 22:48:25 -0800

    feat(graphql-codegen): use graphql-codegen schema-ast to generate root-evel schema.graphql file (#6)

--- api/@types/resolvers.d.ts
+++ api/@types/resolvers.d.ts
@@ -11,13 +11,63 @@ export type Scalars = {
   Boolean: boolean;
   Int: number;
   Float: number;
-  DateTime: any;
   _FieldSet: any;
+  DateTime: any;
 };
 
 
 
 
+
+
+export type Astronaut = {
+  __typename?: 'Astronaut';
+  firstName: Scalars['String'];
+  /** ID is an auto-incrementing value from data store */
+  id: Scalars['ID'];
+  lastName: Scalars['String'];
+  /** Missions flown by astronaut */
+  missions: Array<Maybe<Mission>>;
+};
+
+export type AstronautResult = Astronaut | NotFound;
+
+
+export type Mission = {
+  __typename?: 'Mission';
+  /** Crew members aboard the mission */
+  astronauts: Array<Astronaut>;
+  /** Mapped from cm_name */
+  commandModule: Scalars['String'];
+  /** Duration of the mission in seconds */
+  duration?: Maybe<Scalars['Int']>;
+  id: Scalars['ID'];
+  /**
+   * Launch Date of mission including UTC timestamp
+   * **Example:** `10/11/1968 15:02:00`
+   */
+  launchDate: Scalars['DateTime'];
+  launchVehicle: Scalars['String'];
+  /** Mapped from lm_name */
+  lunarModule: Scalars['String'];
+  /**
+   * Commonly associated with the name.
+   * **Example:** `Apollo 11`
+   */
+  mission: Scalars['String'];
+  /** Mapped from remarks */
+  notes?: Maybe<Scalars['String']>;
+  status: Mission_Status_Enum;
+};
+
+export enum Mission_Status_Enum {
+  Aborted = 'ABORTED',
+  Canceled = 'CANCELED',
+  Completed = 'COMPLETED',
+  Failed = 'FAILED'
+}
+
+export type MissionResult = Mission | NotFound;
 
 export type NotFound = {
   __typename?: 'NotFound';
@@ -26,17 +76,17 @@ export type NotFound = {
 
 export type Query = {
   __typename?: 'Query';
-  hello?: Maybe<Scalars['String']>;
-  /** Fetch all Astronauts from the Apollo space program */
-  astronauts: Array<Maybe<Astronaut>>;
   /**
    * Find an Astronaut by their ID
    * May return a `NotFound`
    */
   astronaut: AstronautResult;
+  /** Fetch all Astronauts from the Apollo space program */
+  astronauts: Array<Maybe<Astronaut>>;
+  hello?: Maybe<Scalars['String']>;
+  mission: MissionResult;
   /** Fetch all Apollo space program missions */
   missions?: Maybe<Array<Maybe<Mission>>>;
-  mission: MissionResult;
 };
 
 
@@ -49,73 +99,10 @@ export type QueryMissionArgs = {
   id: Scalars['Int'];
 };
 
-export type AstronautResult = Astronaut | NotFound;
-
-export type Astronaut = {
-  __typename?: 'Astronaut';
-  /** ID is an auto-incrementing value from data store */
-  id: Scalars['ID'];
-  firstName: Scalars['String'];
-  lastName: Scalars['String'];
-  /** Missions flown by astronaut */
-  missions: Array<Maybe<Mission>>;
-};
-
-export type MissionResult = Mission | NotFound;
-
-export type Mission = {
-  __typename?: 'Mission';
-  id: Scalars['ID'];
-  /**
-   * Commonly associated with the name.
-   * 
-   * **Example:** `Apollo 11`
-   */
-  mission: Scalars['String'];
-  status: Mission_Status_Enum;
-  /**
-   * Launch Date of mission including UTC timestamp
-   * 
-   * **Example:** `10/11/1968 15:02:00`
-   */
-  launchDate: Scalars['DateTime'];
-  /** Crew members aboard the mission */
-  astronauts: Array<Astronaut>;
-  /** Mapped from cm_name */
-  commandModule: Scalars['String'];
-  /** Mapped from lm_name */
-  lunarModule: Scalars['String'];
-  launchVehicle: Scalars['String'];
-  /** Mapped from remarks */
-  notes?: Maybe<Scalars['String']>;
-  /** Duration of the mission in seconds */
-  duration?: Maybe<Scalars['Int']>;
-};
-
-export enum Mission_Status_Enum {
-  Canceled = 'CANCELED',
-  Completed = 'COMPLETED',
-  Aborted = 'ABORTED',
-  Failed = 'FAILED'
-}
-
-
-
 
 
 export type ResolverTypeWrapper<T> = Promise<T> | T;
 
-export type ReferenceResolver<TResult, TReference, TContext> = (
-      reference: TReference,
-      context: TContext,
-      info: GraphQLResolveInfo
-    ) => Promise<TResult> | TResult;
-
-      type ScalarCheck<T, S> = S extends true ? T : NullableCheck<T, S>;
-      type NullableCheck<T, S> = Maybe<T> extends T ? Maybe<ListCheck<NonNullable<T>, S>> : ListCheck<T, S>;
-      type ListCheck<T, S> = T extends (infer U)[] ? NullableCheck<U, S>[] : GraphQLRecursivePick<T, S>;
-      export type GraphQLRecursivePick<T, S> = { [K in keyof T & keyof S]: ScalarCheck<T[K], S[K]> };
-    
 
 export type LegacyStitchingResolver<TResult, TParent, TContext, TArgs> = {
   fragment: string;
@@ -190,33 +177,67 @@ export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs
 
 /** Mapping between all available schema types and the resolvers types */
 export type ResolversTypes = {
-  NotFound: ResolverTypeWrapper<NotFound>;
-  String: ResolverTypeWrapper<Scalars['String']>;
-  Query: ResolverTypeWrapper<{}>;
-  Int: ResolverTypeWrapper<Scalars['Int']>;
-  AstronautResult: ResolversTypes['Astronaut'] | ResolversTypes['NotFound'];
   Astronaut: ResolverTypeWrapper<Astronaut>;
+  String: ResolverTypeWrapper<Scalars['String']>;
   ID: ResolverTypeWrapper<Scalars['ID']>;
-  MissionResult: ResolversTypes['Mission'] | ResolversTypes['NotFound'];
-  Mission: ResolverTypeWrapper<Mission>;
-  MISSION_STATUS_ENUM: Mission_Status_Enum;
+  AstronautResult: ResolversTypes['Astronaut'] | ResolversTypes['NotFound'];
   DateTime: ResolverTypeWrapper<Scalars['DateTime']>;
+  Mission: ResolverTypeWrapper<Mission>;
+  Int: ResolverTypeWrapper<Scalars['Int']>;
+  MISSION_STATUS_ENUM: Mission_Status_Enum;
+  MissionResult: ResolversTypes['Mission'] | ResolversTypes['NotFound'];
+  NotFound: ResolverTypeWrapper<NotFound>;
+  Query: ResolverTypeWrapper<{}>;
   Boolean: ResolverTypeWrapper<Scalars['Boolean']>;
 };
 
 /** Mapping between all available schema types and the resolvers parents */
 export type ResolversParentTypes = {
-  NotFound: NotFound;
-  String: Scalars['String'];
-  Query: {};
-  Int: Scalars['Int'];
-  AstronautResult: ResolversParentTypes['Astronaut'] | ResolversParentTypes['NotFound'];
   Astronaut: Astronaut;
+  String: Scalars['String'];
   ID: Scalars['ID'];
-  MissionResult: ResolversParentTypes['Mission'] | ResolversParentTypes['NotFound'];
-  Mission: Mission;
+  AstronautResult: ResolversParentTypes['Astronaut'] | ResolversParentTypes['NotFound'];
   DateTime: Scalars['DateTime'];
+  Mission: Mission;
+  Int: Scalars['Int'];
+  MissionResult: ResolversParentTypes['Mission'] | ResolversParentTypes['NotFound'];
+  NotFound: NotFound;
+  Query: {};
   Boolean: Scalars['Boolean'];
+};
+
+export type AstronautResolvers<ContextType = any, ParentType extends ResolversParentTypes['Astronaut'] = ResolversParentTypes['Astronaut']> = {
+  firstName?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  lastName?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  missions?: Resolver<Array<Maybe<ResolversTypes['Mission']>>, ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type AstronautResultResolvers<ContextType = any, ParentType extends ResolversParentTypes['AstronautResult'] = ResolversParentTypes['AstronautResult']> = {
+  __resolveType: TypeResolveFn<'Astronaut' | 'NotFound', ParentType, ContextType>;
+};
+
+export interface DateTimeScalarConfig extends GraphQLScalarTypeConfig<ResolversTypes['DateTime'], any> {
+  name: 'DateTime';
+}
+
+export type MissionResolvers<ContextType = any, ParentType extends ResolversParentTypes['Mission'] = ResolversParentTypes['Mission']> = {
+  astronauts?: Resolver<Array<ResolversTypes['Astronaut']>, ParentType, ContextType>;
+  commandModule?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  duration?: Resolver<Maybe<ResolversTypes['Int']>, ParentType, ContextType>;
+  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  launchDate?: Resolver<ResolversTypes['DateTime'], ParentType, ContextType>;
+  launchVehicle?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  lunarModule?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  mission?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  notes?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  status?: Resolver<ResolversTypes['MISSION_STATUS_ENUM'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type MissionResultResolvers<ContextType = any, ParentType extends ResolversParentTypes['MissionResult'] = ResolversParentTypes['MissionResult']> = {
+  __resolveType: TypeResolveFn<'Mission' | 'NotFound', ParentType, ContextType>;
 };
 
 export type NotFoundResolvers<ContextType = any, ParentType extends ResolversParentTypes['NotFound'] = ResolversParentTypes['NotFound']> = {
@@ -225,56 +246,21 @@ export type NotFoundResolvers<ContextType = any, ParentType extends ResolversPar
 };
 
 export type QueryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = {
-  hello?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
-  astronauts?: Resolver<Array<Maybe<ResolversTypes['Astronaut']>>, ParentType, ContextType>;
   astronaut?: Resolver<ResolversTypes['AstronautResult'], ParentType, ContextType, RequireFields<QueryAstronautArgs, never>>;
-  missions?: Resolver<Maybe<Array<Maybe<ResolversTypes['Mission']>>>, ParentType, ContextType>;
+  astronauts?: Resolver<Array<Maybe<ResolversTypes['Astronaut']>>, ParentType, ContextType>;
+  hello?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
   mission?: Resolver<ResolversTypes['MissionResult'], ParentType, ContextType, RequireFields<QueryMissionArgs, 'id'>>;
+  missions?: Resolver<Maybe<Array<Maybe<ResolversTypes['Mission']>>>, ParentType, ContextType>;
 };
-
-export type AstronautResultResolvers<ContextType = any, ParentType extends ResolversParentTypes['AstronautResult'] = ResolversParentTypes['AstronautResult']> = {
-  __resolveType: TypeResolveFn<'Astronaut' | 'NotFound', ParentType, ContextType>;
-};
-
-export type AstronautResolvers<ContextType = any, ParentType extends ResolversParentTypes['Astronaut'] = ResolversParentTypes['Astronaut']> = {
-  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
-  firstName?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
-  lastName?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
-  missions?: Resolver<Array<Maybe<ResolversTypes['Mission']>>, ParentType, ContextType>;
-  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
-};
-
-export type MissionResultResolvers<ContextType = any, ParentType extends ResolversParentTypes['MissionResult'] = ResolversParentTypes['MissionResult']> = {
-  __resolveType: TypeResolveFn<'Mission' | 'NotFound', ParentType, ContextType>;
-};
-
-export type MissionResolvers<ContextType = any, ParentType extends ResolversParentTypes['Mission'] = ResolversParentTypes['Mission']> = {
-  __resolveReference?: ReferenceResolver<Maybe<ResolversTypes['Mission']>, { __typename: 'Mission' } & (GraphQLRecursivePick<ParentType, {"id":true}> | GraphQLRecursivePick<ParentType, {"mission":true}>), ContextType>;
-  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
-  mission?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
-  status?: Resolver<ResolversTypes['MISSION_STATUS_ENUM'], ParentType, ContextType>;
-  launchDate?: Resolver<ResolversTypes['DateTime'], ParentType, ContextType>;
-  astronauts?: Resolver<Array<ResolversTypes['Astronaut']>, ParentType, ContextType>;
-  commandModule?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
-  lunarModule?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
-  launchVehicle?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
-  notes?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
-  duration?: Resolver<Maybe<ResolversTypes['Int']>, ParentType, ContextType>;
-  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
-};
-
-export interface DateTimeScalarConfig extends GraphQLScalarTypeConfig<ResolversTypes['DateTime'], any> {
-  name: 'DateTime';
-}
 
 export type Resolvers<ContextType = any> = {
+  Astronaut?: AstronautResolvers<ContextType>;
+  AstronautResult?: AstronautResultResolvers<ContextType>;
+  DateTime?: GraphQLScalarType;
+  Mission?: MissionResolvers<ContextType>;
+  MissionResult?: MissionResultResolvers<ContextType>;
   NotFound?: NotFoundResolvers<ContextType>;
   Query?: QueryResolvers<ContextType>;
-  AstronautResult?: AstronautResultResolvers<ContextType>;
-  Astronaut?: AstronautResolvers<ContextType>;
-  MissionResult?: MissionResultResolvers<ContextType>;
-  Mission?: MissionResolvers<ContextType>;
-  DateTime?: GraphQLScalarType;
 };
 
 
